28f6a4cd3588703c746eae32e49937584ce03aa7
quantu-adunati <mcfaddenr.ebb@gmail.com>
2019-03-12 19:13:48 +0200

    Searching for userid that appears the most then getting the index was added

--- FR.py
+++ FR.py
@@ -10,7 +10,25 @@ def AuthenticateUser(aArrImg):
     lPercentages = []
     for img in aArrImg:
         userID, percent = AuthenticateImage(img) #This function is implemented elsewhere
+        lUserIDs.append(userID)
+        lPercentages.append(percent)
+    lCount = []
 
-    ind = -1
-    Log(lUserIDs[ind],start, end, status)
-    return lUserIDs[ind]  # TODO fix
+    for id in lUserIDs.copy():
+        print(id)
+        lCount.append(0)
+        for id2 in lUserIDs.copy():
+            if id == id2:
+                index = lUserIDs.index(id)
+                lCount[index] += 1
+
+  
+
+    end = time.time()
+    if lUserIDs[ind] == -1:
+        status = False
+    else:
+        status = True
+    Log(lUserIDs[ind],start, end, status) # call Log() which logs the time,status of finding and the userId(-1 if not found, Most likely when status is false)
+    # TODO call log
+    print('======')
