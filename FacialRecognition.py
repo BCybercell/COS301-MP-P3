d58cffa39079ee645b146bea3e71e6b4f860dddb
ItsMeKingk <kolivier12@gmail.com>
2019-03-12 22:27:07 +0200

    Got my random numbers to work

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -4,3 +4,7 @@ import json as json
 
 # TODO Kyle
 def AuthenticateImage(aImg):
+	# print(aImg);
+	lChance = Rand.randint(1, 100) # TODO need to make this the actual facial recognition function library
+	
+	lUser = Rand.randint(1, 10) #userID just 10 users for now as example
