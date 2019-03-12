67e10f218a48a23bbe071fd7d868ca641425301e
Deane Roos <blaze.cybercell@gmail.com>
2019-03-12 13:23:13 +0200

    Added skeleton code for FR.py

---
+++ FacialRecognition.py
@@ -0,0 +1,22 @@
+import time as time
+import random as Rand
+import json as json
+
+#  TODO Richard
+def AuthenticateUser(aArrImg):
+
+
+
+
+# TODO Kyle
+def AuthenticateImage(aImg):
+
+
+
+# TODO Tegan
+def Log(aUserID, aStart, aEnd, aStatus):
+
+
+
+#  TODO Deane
+def Update():
