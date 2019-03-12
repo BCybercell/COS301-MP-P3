157fca3dd32d7f383ee1ccdcb2e5d4ff47290d6a
quantu-adunati <mcfaddenr.ebb@gmail.com>
2019-03-12 19:10:09 +0200

    Added the Log() call, Almost forgot about this.

--- FR.py
+++ FR.py
@@ -4,11 +4,13 @@ import json as json
 
 #  TODO Richard
 def AuthenticateUser(aArrImg):
-    
+    Update() #Call Update function to get new/updated list of the database from CIS
     start = time.time()
     lUserIDs = []
     lPercentages = []
     for img in aArrImg:
         userID, percent = AuthenticateImage(img) #This function is implemented elsewhere
 
+    ind = -1
+    Log(lUserIDs[ind],start, end, status)
     return lUserIDs[ind]  # TODO fix
