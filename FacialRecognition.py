3acfe05c731ece4175efdf1cb2705f9556e945a2
ItsMeKingk <kolivier12@gmail.com>
2019-03-13 12:24:13 +0200

    Added in the return values of userID and matchPercent

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -8,3 +8,7 @@ def AuthenticateImage(aImg):
 	lChance = Rand.randint(1, 100) # TODO need to make this the actual facial recognition function library
 	
 	lUser = Rand.randint(1, 10) #userID just 10 users for now as example
+	if lChance < 50: 	#TODO change to 85 after demo as must be 85% match
+		lUser = -1		#userID of -1 means it failed for this demo purpose
+	
+	return lUser, lChance
