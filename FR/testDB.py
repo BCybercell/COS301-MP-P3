0c15941b1527f1df75d323a6b9845ed82fb8e197
quantu-adunati <mcfaddenr.ebb@gmail.com>
2019-03-29 13:44:44 +0200

    Updated test.db to store the image as base64

--- FR/testDB.py
+++ FR/testDB.py
@@ -1,27 +1,34 @@
 import pymongo 
+import base64
+import gridfs
 
 myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
 
 mydb = myclient["testDB"]
-mycol = mydb["Clients"]
+mycol = mydb["work"]
+strr=[]
+for i in range(6):
+  s = "./"+str(i+1)+".jpg"
+  with open(s, "rb") as imageFile:
+       encoded_string = base64.b64encode(imageFile.read())
+       strr.append(encoded_string)
 
 exampleActiveUser = {
-    "userID" : "0", #ID should be stored as a string
-    "status" : True,
-    "photos" : [
-      "./1.jpg", "./2.jpg","./6.jpg","./9.jpg","./15.jpg" 
-    ]
-}
-
+        "userID" : "0", #ID should be stored as a string
+        "status" : True,
+        "photos" : [
+            strr[0],strr[1],strr[2],strr[3],strr[4]
+        ]
+    }
 x = mycol.insert_one(exampleActiveUser)
-print("Active user example: " + str(exampleActiveUser))
-exampleActiveUserTwo = {
-    "userID" : "1", #ID should be stored as a string
-    "status" : True,
-    "photos" : [
-      "./8.jpg", "./10.jpg","./12.jpg" 
-    ]
-}
-x = mycol.insert_one(exampleActiveUserTwo)
+# print("Active user example: " + str(exampleActiveUser))
+# exampleActiveUserTwo = {
+#     "userID" : "1", #ID should be stored as a string
+#     "status" : True,
+#     "photos" : [
+#       "./8.jpg", "./10.jpg","./12.jpg" 
+#     ]
+# }
+# x = mycol.insert_one(exampleActiveUserTwo)
 
-print("Active user example: " + str(exampleActiveUserTwo))
+# print("Active user example: " + str(exampleActiveUserTwo))
