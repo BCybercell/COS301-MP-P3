d5ff923ad940464032d24d2d1dafe5064afca1fc
Deane Roos <blaze.cybercell@gmail.com>
2019-04-02 11:27:49 +0200

    merger

--- FR/main.py
+++ FR/main.py
@@ -49,7 +49,7 @@ def UpImage(request):
 def AuthUser(request):
     if request.method == 'GET':
         lImg = request.GET['Image']
-        lUserID = AuthenticateUser(lImg)
+        lUserID = AuthenticateUser(request)
         if lUserID > 0:
             return JsonResponse({'UserID': lUserID})
         else:
