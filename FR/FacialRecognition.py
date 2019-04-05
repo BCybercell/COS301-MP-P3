7995a6f643258ae6cc9fde0f662ee80bb69a13cd
Deane Roos <blaze.cybercell@gmail.com>
2019-04-05 11:04:39 +0200

    I can't

--- FR/FacialRecognition.py
+++ FR/FacialRecognition.py
@@ -40,7 +40,7 @@ def AddImages(userID, aArrImg):     #TODO Kyle
         if key.get("userID") == userID:
             #for img in aArrImg:
             encoded_string = base64.b64encode(aArrImg.read())
-            encoding = (np.array(face_recognition.face_encodings(face_recognition.load_image_file(aArrImg.read()))[0]).tolist())
+            encoding = (np.array(face_recognition.face_encodings(face_recognition.load_image_file(aArrImg))[0]).tolist())
                     #strr.append(encoded_string)
                     #myquery = {"userID": str(userID)}
                     #newvalues = {"$push": {"photos": strr}}
