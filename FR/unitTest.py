90987e1d374639c3d242e3c23110f4b82f0d768b
Deane Roos <blaze.cybercell@gmail.com>
2019-04-05 10:50:39 +0200

    Misplacement

--- FR/unitTest.py
+++ FR/unitTest.py
@@ -10,17 +10,13 @@ class TestLog(unittest.TestCase):
         self.fail()
 
     def test_Log_TrueStatus(self):
-        lStart = dt.datetime(2019, 3, 12, 18, 00, 00)
-        lEnd = dt.datetime(2019, 3, 12, 18, 39, 00)
-        self.assertTrue(Log(0, lStart, lEnd, True), "Should be True")
+        self.assertTrue(Log("0",True), "Should be True")
 
     def test_Log_FalseStatus(self):
         self.fail()
 
     def test_Log_FalseStatus(self):
-        lStart = dt.datetime(2019, 3, 12, 18, 00, 00)
-        lEnd = dt.datetime(2019, 3, 12, 18, 39, 00)
-        self.assertTrue(Log(1, lStart, lEnd, False), "Should be True")
+        self.assertTrue(Log("0",False), "Should be True")
 
 
 # class TestGetLog(unittest.TestCase):
